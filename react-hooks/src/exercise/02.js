5b1d5ae704ccae49f1e9ebdd1a6f9dc3e81ecd57
Mendel Bakaleynik <mendelbak@gmail.com>
2022-06-30 20:20:05 -0400

    Cloned react-hooks repo. Finished exercise 02

--- react-hooks/src/exercise/02.js
+++ react-hooks/src/exercise/02.js
@@ -1,20 +1,27 @@
 // useEffect: persistent state
 // http://localhost:3000/isolated/exercise/02.js
 
-import * as React from 'react'
+import * as React from 'react';
+
+function useLocalStorageState(key, defaultValue = '') {
+  const [state, setState] = React.useState(
+    localStorage.getItem(key) || defaultValue,
+  );
+
+  React.useEffect(() => {
+    localStorage.setItem(key, state);
+  }, [key, state]);
+
+  return [state, setState];
+}
 
 function Greeting({initialName = ''}) {
-  // 🐨 initialize the state to the value from localStorage
-  // 💰 window.localStorage.getItem('name') ?? initialName
-  const [name, setName] = React.useState(initialName)
-
-  // 🐨 Here's where you'll use `React.useEffect`.
-  // The callback should set the `name` in localStorage.
-  // 💰 window.localStorage.setItem('name', name)
+  const [name, setName] = useLocalStorageState('name', initialName);
 
   function handleChange(event) {
-    setName(event.target.value)
+    setName(event.target.value);
   }
+
   return (
     <div>
       <form>
@@ -23,11 +30,11 @@ function Greeting({initialName = ''}) {
       </form>
       {name ? <strong>Hello {name}</strong> : 'Please type your name'}
     </div>
-  )
+  );
 }
 
 function App() {
-  return <Greeting />
+  return <Greeting />;
 }
 
-export default App
+export default App;
